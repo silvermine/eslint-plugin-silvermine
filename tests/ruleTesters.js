3c56be60ed2d35437387dba8abe03be45af1bbd9
Fabrice Ketchakeu <fabriceursil@gmail.com>
2019-12-25 16:41:27 -0500

    chore: Upgrade to eslint 6

--- tests/ruleTesters.js
+++ tests/ruleTesters.js
@@ -7,7 +7,7 @@ module.exports = {
    es6: function() {
       return new RuleTester({
          parserOptions: {
-            'ecmaVersion': 2018,
+            'ecmaVersion': 2019,
          },
          env: {
             es6: true,
@@ -17,9 +17,9 @@ module.exports = {
 
    typeScript: function() {
       return new RuleTester({
-         parser: 'typescript-eslint-parser',
+         parser: require.resolve('@typescript-eslint/parser'),
          parserOptions: {
-            'ecmaVersion': 2018,
+            'ecmaVersion': 2019,
             'sourceType': 'module',
             // Disable warning banner for possibly incompatible versions of TypeScript
             'loggerFn': false,
